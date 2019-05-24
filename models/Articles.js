8a61578269e8de38c9cf91086bde60d6136cbcbc
Brittany F <brittfranklin92@gmail.com>
2019-05-24 19:53:12 -0400

    More mongo updates

---
+++ models/Articles.js
@@ -0,0 +1,34 @@
+import { model } from "mongoose";
+
+import { Schema } from "mongoose";
+
+import { type } from "os";
+
+import { truncate } from "fs";
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ArticlesSchema = new Schema({
+  headline: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  notes: {
+    type: Schema.Types.ObjectId,
+    ref: 'Notes'
+  }
+});
+
+var Articles = mongoose.model('Articles', ArticlesSchema);
+
+module.exports = Articles;
