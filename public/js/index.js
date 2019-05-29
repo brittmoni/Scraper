04eb4a81198917c0467982c794209edd287048e3
Brittany F <brittfranklin92@gmail.com>
2019-05-28 21:52:17 -0400

    Small changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,5 @@
-$.getJSON('/articles', function)
+$.getJSON('/articles', function(data){
+  data.forEach(function(data){
+    $('article-display').append(`<p data-id='${data[i]._id}> ${data[i].title} <br/> ${data[i].link}</p>`);
+  });
+});
